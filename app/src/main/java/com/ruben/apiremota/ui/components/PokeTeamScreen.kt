76b52c71229b810308ba271feb8884599a84e29a
Raúl <raulpg@iescastelar.com>
2023-02-15 19:43:42 +0100

    pokemon team screen created

---
+++ app/src/main/java/com/ruben/apiremota/ui/components/PokeTeamScreen.kt
@@ -0,0 +1,13 @@
+package com.ruben.apiremota.ui.components
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.font.FontWeight
+import androidx.navigation.NavController
+import com.ruben.apiremota.presentation.PokemonViewModel
+
+@Composable
+fun PokeTeamScreen (viewModel: PokemonViewModel, navController: NavController) {
+    Text(text = "Tu Equipo Pokémon", fontWeight = FontWeight. Bold)
+    //TODO: To be continued...
+}
