81eba230e6d3017793fca4a43f7f7601d0c2ff24
Raúl <raulpg@iescastelar.com>
2023-02-16 08:32:30 +0100

    Updated version of pokemon team screen, but still needs changes

--- app/src/main/java/com/ruben/apiremota/ui/components/PokeTeamScreen.kt
+++ app/src/main/java/com/ruben/apiremota/ui/components/PokeTeamScreen.kt
@@ -1,13 +1,48 @@
 package com.ruben.apiremota.ui.components
 
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.text.font.FontWeight
-import androidx.navigation.NavController
-import com.ruben.apiremota.presentation.PokemonViewModel
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.ruben.apiremota.data.remote.Pokemon
+
+val pokemonList = listOf(
+    Pokemon(100, 30,1,"Pikachu",1,50),
+)
 
 @Composable
-fun PokeTeamScreen (viewModel: PokemonViewModel, navController: NavController) {
-    Text(text = "Tu Equipo Pokémon", fontWeight = FontWeight. Bold)
-    //TODO: To be continued...
+    fun PokemonCard(pokemon: Pokemon) {
+        Card(
+            backgroundColor = Color.White,
+            elevation = 4.dp,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+        ) {
+            Column(
+                modifier = Modifier.padding(16.dp)
+            ) {
+                Text(text = pokemon.name, style = MaterialTheme.typography.h5)
+            }
+        }
+    }
+
+    @Composable
+    fun PokemonList(pokemonList: List<Pokemon>) {
+        Column {
+            for (pokemon in pokemonList) {
+                PokemonCard(pokemon = pokemon)
+            }
+        }
+    }
+
+@Composable
+fun SixPokemonScreen() {
+    Scaffold(
+        topBar = { TopAppBar(title = { Text(text = "Tu Equipo Pokémon") }) }
+    ) {
+        PokemonList(pokemonList = pokemonList)
+    }
 }
