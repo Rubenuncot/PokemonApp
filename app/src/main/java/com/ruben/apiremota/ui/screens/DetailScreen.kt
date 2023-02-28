92b1d1ec7b6ebc8a1001fada780fcfee54651ba9
Ruben <Ruben@DESKTOP-MUMHU9R>
2023-02-28 17:26:43 +0100

    Modificado el detailScreen para que se vea el texto en el centro

--- app/src/main/java/com/ruben/apiremota/ui/screens/DetailScreen.kt
+++ app/src/main/java/com/ruben/apiremota/ui/screens/DetailScreen.kt
@@ -20,14 +20,14 @@ import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 
 @Composable
-fun DetailScreen(id: Int , name: String , height: Int, weight: Int, description: String) {
-    val offset = Offset(3.0f,8.0f)
+fun DetailScreen(id: Int, name: String, height: Int, weight: Int, description: String) {
+    val offset = Offset(3.0f, 8.0f)
     Box(
         modifier = Modifier
             .fillMaxSize()
             .background(Color.LightGray),
         contentAlignment = Alignment.TopCenter
-    ){
+    ) {
         Column(Modifier.padding(20.dp)) {
             Card(
                 modifier = Modifier
@@ -37,28 +37,60 @@ fun DetailScreen(id: Int , name: String , height: Int, weight: Int, description:
                 backgroundColor = Color(0xFFECE3F6)
             ) {
                 Column() {
-                    Text(text = "Pokemon Details",modifier = Modifier.width(350.dp), style = TextStyle(textAlign = TextAlign.Center,fontSize = 35.sp,fontWeight = FontWeight.Bold,color = Color.Black, shadow = Shadow(
-                        color = Color.White, offset = offset, blurRadius = 2f
-                    )))
-                    Text(text = "$name".uppercase(),textAlign = TextAlign.Center,modifier = Modifier.width(350.dp), fontSize = 20.sp, fontWeight = FontWeight.Bold)
-                    AsyncImage(model = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/$id.png",
-                        contentDescription = "Image pokemon : $id",modifier = Modifier.width(350.dp))
-                    Row (
-                        horizontalArrangement = Arrangement.Center
-                    ){
-                        Column(
-                            horizontalAlignment = Alignment.CenterHorizontally,
-                        ) {
-                            Text(text = "Pokedex Number #: $id", style = TextStyle(textAlign = TextAlign.Center))
-                            Text(text = "Height: $height", style = TextStyle(textAlign = TextAlign.Center))
-                            Text(text = "Weight: $weight", style = TextStyle(textAlign = TextAlign.Center))
-                            Text(text = "Description: $description", style = TextStyle(textAlign = TextAlign.Center))
-                        }
-                    }
-
+                    Text(
+                        text = "Pokemon Details",
+                        modifier = Modifier.width(350.dp),
+                        style = TextStyle(
+                            textAlign = TextAlign.Center,
+                            fontSize = 35.sp,
+                            fontWeight = FontWeight.Bold,
+                            color = Color.Black,
+                            shadow = Shadow(
+                                color = Color.White, offset = offset, blurRadius = 2f
+                            )
+                        )
+                    )
+                    Text(
+                        text = "$name".uppercase(),
+                        textAlign = TextAlign.Center,
+                        modifier = Modifier.width(350.dp),
+                        fontSize = 20.sp,
+                        fontWeight = FontWeight.Bold
+                    )
+                    AsyncImage(
+                        model = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/$id.png",
+                        contentDescription = "Image pokemon : $id",
+                        modifier = Modifier.width(350.dp)
+                    )
                 }
-
+                Row(
+                    horizontalArrangement = Arrangement.Center,
+                    verticalAlignment = Alignment.Bottom,
+                    modifier = Modifier.padding(bottom = 60.dp)
+                ) {
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                    ) {
+                        Text(
+                            text = "Pokedex Number: #$id",
+                            style = TextStyle(textAlign = TextAlign.Center)
+                        )
+                        Text(
+                            text = "Height: $height",
+                            style = TextStyle(textAlign = TextAlign.Center)
+                        )
+                        Text(
+                            text = "Weight: $weight",
+                            style = TextStyle(textAlign = TextAlign.Center)
+                        )
+                        Text(
+                            text = "Description: $description",
+                            style = TextStyle(textAlign = TextAlign.Center)
+                        )
+                    }
+                }
             }
         }
     }
 }
+
